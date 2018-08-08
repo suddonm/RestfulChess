0fa3cb0ae405679a35669227f0f4e450391d7228
Mark Suddon <suddonm@gmail.com>
2018-08-07 21:28:25 -0700

    Add to repo

---
+++ RestfulChess/Models/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestfulChess.Models
+{
+    public class Game
+    {
+        public long Id { get; set; }
+        public string MoveList { get; set; }
+
+        public Game()
+        {
+
+        }
+    }
+}
